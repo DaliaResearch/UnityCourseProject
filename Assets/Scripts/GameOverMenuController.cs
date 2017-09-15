714ee4c8ef98c3a7b95377c0885a15f47172888c
Fernando Guillen <fguillen.mail@gmail.com>
2017-09-16 00:57:34 +0200

    Lesson 72: GameOver Menu

---
+++ Assets/Scripts/GameOverMenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenuController : MonoBehaviour {
+
+	private LevelManager theLevelManager;
+
+	// Use this for initialization
+	void Start () {
+		theLevelManager = FindObjectOfType<LevelManager> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void RestartLevel () {
+		PlayerPrefs.SetInt ("CoinsCount", 0);
+		PlayerPrefs.SetInt ("actualLifes", theLevelManager.maxLifes);
+		PlayerPrefs.SetInt ("actualHealth", theLevelManager.maxHealth);
+
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+	}
+
+	public void MainMenu (){
+		SceneManager.LoadScene ("MainMenu");
+	}
+}

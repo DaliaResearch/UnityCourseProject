98d23c27e605106fb98da41a5ae686c6f22d70fa
Fernando Guillen <fguillen.mail@gmail.com>
2017-09-16 01:26:22 +0200

    Lesson 73: Pause Screen

---
+++ Assets/Scripts/PauseScreenController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScreenController : MonoBehaviour {
+
+	public GameObject thePauseScreen;
+
+	// Use this for initialization
+	void Start () {
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.P)) {
+			thePauseScreen.SetActive (true);
+		}
+	}
+
+	public void ResumeGame () {
+		thePauseScreen.SetActive (false);
+	}
+
+	public void MainMenu () {
+		SceneManager.LoadScene ("MainMenu");
+	}
+}

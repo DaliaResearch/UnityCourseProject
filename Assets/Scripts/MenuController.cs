d2919580778d777d61e1686d1c69f605b62c44c8
Fernando Guillen <fguillen.mail@gmail.com>
2017-09-15 21:51:27 +0200

    Lesson 71: Menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+	public string firstLevel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void NewGame () {
+		PlayerPrefs.DeleteAll ();
+
+		SceneManager.LoadScene (firstLevel);}
+
+	public void ContinueGame () {
+
+		SceneManager.LoadScene ("LevelSelector");
+	}
+
+	public void Quit () {
+		Application.Quit ();
+	}
+}
